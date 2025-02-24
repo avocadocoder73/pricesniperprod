75e6b1b9afcdd061738167afb6d1c585cdd7bca2
avocadocoder73 <113757969+avocadocoder73@users.noreply.github.com>
2025-02-24 12:21:13 -0600

    bing

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -44,6 +44,7 @@ export default function RootLayout({
     
     <html lang="en" className="min-h-screen">
       <head>
+        <meta name="msvalidate.01" content="DBB729304426E515E9D566D7AC6AC89B" />
         <Script src={`https://www.googletagmanager.com/gtag/js?id=G-YDY00DSFXS`} strategy="afterInteractive"/>
         <Script id="google-analytics" strategy="afterInteractive">
           {
